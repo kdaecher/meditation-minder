ea513a0b829dcd5a1f8fae8b6ee5b7148203326d
Karson Daecher <kbd45@cornell.edu>
2024-04-19 10:31:04 -0400

    init + basic timer

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import './App.css'
+import { useRef, useState } from 'react';
+
+const MIN_TIME = 7;
+const MAX_TIME = 10;
+
+function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const timerRef = useRef<number>();
+  const [percentPassed, setPercentPassed] = useState(0);
+  let interval = 0;
+  let endTime = 0;
+
+  const updateTime = () => {
+    const currentTime = Date.now();
+    if (currentTime < endTime) {
+      setPercentPassed(100 - (((endTime - currentTime)/(interval * 60 * 1000))* 100));
+    } else {
+      clearInterval(timerRef.current);
+      setIsRunning(false);
+      setPercentPassed(0);
+    }
+  };
+
+  const runTimer = () => {
+    interval = Math.floor(Math.random() * (MAX_TIME - MIN_TIME + 1) + MIN_TIME)
+    endTime = Date.now() + (interval * 60 * 1000);
+    setIsRunning(true);
+    timerRef.current = setInterval(updateTime, 1000);
+  };
+  
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      {!isRunning ? (
+        <button onClick={runTimer}>
+          Start
+        </button>
+      ) : (
+        <div
+          style={{
+            height: `${percentPassed}%`,
+            width: '100%',
+            backgroundColor: 'green'
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App
